2022a3a08ca93e4c8fe4153015dd73ebd76abebe
Beatle95 <tishenko.vlad.apc@gmail.com>
2025-07-20 23:02:45 +0300

    Fix version embedding

--- build_all.sh
+++ build_all.sh
@@ -1,3 +1,3 @@
-version=$(git describe --tags)
+version=$(git describe --abbrev=0)
 go build -o ${PWD}/out/server -ldflags "-X 'internal.applicationVersionString=$version'"
 go build -C client_integration_tests -o ${PWD}/out/client_integration_tests -ldflags "-X 'internal.applicationVersionString=$version'"
